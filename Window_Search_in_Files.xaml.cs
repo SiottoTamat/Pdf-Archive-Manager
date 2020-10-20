309dc84b5103d3ac4c0597d12d8b36f552b00c69
Andrea Siotto <siotto.andrea@gmail.com>
2020-10-20 18:05:11 -0400

    improved the search function with output

--- Window_Search_in_Files.xaml.cs
+++ Window_Search_in_Files.xaml.cs
@@ -28,23 +28,52 @@ namespace WPF_PDF_Organizer
         #region Tools
     List<MainWindow.FoundItem> Search_In_Text(string folder, string searchstring, bool subdir=false)
         {
+            StackPanel_Result_Search.Children.Clear();
     foreach (string filename in Directory.GetFiles(folder))
             {
                 FileInfo file = new FileInfo(filename);
                 if (file.Extension == ".txt")
                 {
 
-                
-                string text = System.IO.File.ReadAllText(filename);
-                Match m = Regex.Match(text, searchstring, RegexOptions.IgnoreCase);
-                if (m.Success)
+
+                    string text = System.IO.File.ReadAllText(filename);
+                    string[] pages = Split_Pages(text);
+                    Regex rgx = new Regex(searchstring);
+                    for (int i=0; i<pages.Length; i++)
+                    {
+                        //MessageBox.Show(pages[i]);
+                        
+                        foreach (Match m in rgx.Matches(pages[i]))
+                        {
+                            int max, min;
+                            if(m.Index - 120 < 0) { min = 0; } else { min = m.Index - 120; }
+                            if(m.Index + 120> pages[i].Length) { max = pages[i].Length-m.Index; } else { max = 240; }
+                            string area = pages[i].Substring(min, max);
+                            MessageBox.Show($"In {file.Name} at page {i}: found '{m.Value}' at position {m.Index}.{Environment.NewLine}...{area}...");
+                            SearchItem item = new SearchItem();
+                            item.Label_NameFile.Content = file.Name;
+                            item.Label_Page.Content = i.ToString();
+                            item.RichTextBox_search.Document.Blocks.Add( new Paragraph(new Run($"...{area}...")));
+                            StackPanel_Result_Search.Children.Add(item);
+                        }
+                    }
+                    
+                    
+                }
+                /*Match m = Regex.Match(text, searchstring, RegexOptions.IgnoreCase);
+                //if (m.Success)
                 {
 
                 }
                     MessageBox.Show($"Found '{m.Value}' at position {m.Index}.");
-                }
+                }*/
             }
             return null;
+        }
+        string[] Split_Pages(string text)
+        {
+            string[] result = Regex.Split(text, "---------\\d---------");
+            return result;
         }
 
         #endregion
