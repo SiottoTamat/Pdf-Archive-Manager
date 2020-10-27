6af5b644bede9e0516be7f47a8f6178178a9ff14
SiottoTamat <siotto.andrea@gmail.com>
2020-10-27 07:20:41 -0400

    corrected the rtf save

--- Window_Search_in_Files.xaml.cs
+++ Window_Search_in_Files.xaml.cs
@@ -45,95 +45,222 @@ namespace WPF_PDF_Organizer
                 Task.Run(() =>
                 {
 
-                
-                foreach (string filename in files)
-                {
-                  Dispatcher.Invoke(new Action(() =>
+
+                    foreach (string filename in files)
                     {
-                        index++;
-                        string counter = $"{index} of {nfiles} files.";
-                        Label_Counter.Content = counter;
-                        
-                    }));
-                    FileInfo file = new FileInfo(filename);
+                        Dispatcher.Invoke(new Action(() =>
+                          {
+                              index++;
+                              string counter = $"{index} of {nfiles} files.";
+                              Label_Counter.Content = counter;
+
+                          }));
+                        FileInfo file = new FileInfo(filename);
 
 
-                    string text = System.IO.File.ReadAllText(filename);
-                    string[] pages = Split_Pages(text);
-                    Regex rgx = new Regex(searchstring, RegexOptions.IgnoreCase);
+                        string text = System.IO.File.ReadAllText(filename);
+                        string[] pages = Split_Pages(text);
+                        Regex rgx = new Regex(searchstring, RegexOptions.IgnoreCase);
 
-                    for (int i = 0; i < pages.Length; i++)
-                    {
-                        foreach (Match m in rgx.Matches(pages[i]))
+                        for (int i = 0; i < pages.Length; i++)
                         {
+                            foreach (Match m in rgx.Matches(pages[i]))
+                            {
 
-                            string pre_string = pages[i].Substring(0, m.Index);
-                            string post_string = pages[i].Substring(m.Index + m.Length);
+                                string pre_string = pages[i].Substring(0, m.Index);
+                                string post_string = pages[i].Substring(m.Index + m.Length);
 
-                            if (pre_string.Length > 200) { pre_string = pre_string.Substring(pre_string.Length - 200); } else { }
-                            if (post_string.Length > 200) { post_string = post_string.Substring(0, 200); }
+                                if (pre_string.Length > 200) { pre_string = pre_string.Substring(pre_string.Length - 200); } else { }
+                                if (post_string.Length > 200) { post_string = post_string.Substring(0, 200); }
                                 Dispatcher.Invoke(new Action(() =>
                                 {
                                     SearchItem item = new SearchItem();
                                     item.file = file;
-                                
-                            
 
-                            TextBlock buttonTextBlock = (TextBlock)item.Button_Namefile.FindName("Button_Namefile_TextBlock");
-                            TextBlock buttonTextBlockPdf = (TextBlock)item.Button_Namefile.FindName("Button_Namefile_TextBlock_pdf");
-                            buttonTextBlock.Text = file.Name;
-                            string pdfFullFileName = (file.FullName).Replace(".txt", ".pdf");
-                            if (File.Exists(pdfFullFileName))
-                            {
-                                string pdfFileName = (file.Name).Replace(".txt", ".pdf");
-                                buttonTextBlockPdf.Text = pdfFileName;
-                            }
-                            else
-                            {
-                                buttonTextBlockPdf.Text = "No pdf version of this file.";
-                            }
 
-                            item.Label_Page.Content = "Page " + (i + 1).ToString();
-                                
-                            Paragraph paragraph = new Paragraph();
-                            paragraph.Inlines.Add("...");
-                            paragraph.Inlines.Add(pre_string);
-                            paragraph.Inlines.Add(new Run(m.Value) { FontWeight = FontWeights.Bold, Background = Brushes.Yellow });
-                            paragraph.Inlines.Add(post_string);
-                            paragraph.Inlines.Add("...");
-                            item.RichTextBox_search.Document.Blocks.Clear();
-                            item.RichTextBox_search.Document.Blocks.Add(paragraph);
-                            StackPanel_Result_Search.Children.Add(item);
+
+                                    TextBlock buttonTextBlock = (TextBlock)item.Button_Namefile.FindName("Button_Namefile_TextBlock");
+                                    TextBlock buttonTextBlockPdf = (TextBlock)item.Button_Namefile.FindName("Button_Namefile_TextBlock_pdf");
+                                    buttonTextBlock.Text = file.Name;
+                                    string pdfFullFileName = (file.FullName).Replace(".txt", ".pdf");
+                                    if (File.Exists(pdfFullFileName))
+                                    {
+                                        string pdfFileName = (file.Name).Replace(".txt", ".pdf");
+                                        buttonTextBlockPdf.Text = pdfFileName;
+                                    }
+                                    else
+                                    {
+                                        buttonTextBlockPdf.Text = "No pdf version of this file.";
+                                    }
+
+                                    item.Label_Page.Content = "Page " + (i + 1).ToString();
+
+                                    Paragraph paragraph = new Paragraph();
+                                    paragraph.Inlines.Add("...");
+                                    paragraph.Inlines.Add(pre_string);
+                                    paragraph.Inlines.Add(new Run(m.Value) { FontWeight = FontWeights.Bold, Background = Brushes.Yellow });
+                                    paragraph.Inlines.Add(post_string);
+                                    paragraph.Inlines.Add("...");
+                                    item.RichTextBox_search.Document.Blocks.Clear();
+                                    item.RichTextBox_search.Document.Blocks.Add(paragraph);
+                                    StackPanel_Result_Search.Children.Add(item);
                                 }));
                             }
-                    }
+                        }
                         Dispatcher.Invoke(new Action(() =>
                         {
                             Label_Nfinds.Content = StackPanel_Result_Search.Children.Count;
                         }));
 
 
-                }
+                    }
 
-                string[] Split_Pages(string text)
-                {
-                    string[] result = Regex.Split(text, "---------\\d---------");
-                    return result;
-                }
+                    string[] Split_Pages(string text)
+                    {
+                        string[] result = Regex.Split(text, "---------\\d---------");
+                        return result;
+                    }
                 });
             }
             catch { }
 
         }
+
+        public static void CloneDocument(FlowDocument from, FlowDocument to)
+        {
+            TextRange range = new TextRange(from.ContentStart, from.ContentEnd);
+            MemoryStream stream = new MemoryStream();
+            //System.Windows.Markup.XamlWriter.Save(range, stream);
+            range.Save(stream, DataFormats.Rtf);
+            TextRange range2 = new TextRange(to.ContentEnd, to.ContentEnd);
+            range2.Load(stream, DataFormats.Rtf);
+        }
         #endregion
 
 
         #region ButtonsAndClicks
-        public void Button_TestCreateItem_Click(object sender, RoutedEventArgs e)
+        public void Button_Search_Click(object sender, RoutedEventArgs e)
         {
-            SearchItem item = new SearchItem();
             Search_In_Text(TextBox_Directory.Text, TextBox_Search.Text);
         }
+        private void Menuitem_SaveAs_Rtf(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            saveFileDialog.DefaultExt = "rtf";
+            saveFileDialog.AddExtension = true;
+            List<SearchItem> copylist = new List<SearchItem>();
+             foreach (SearchItem item in StackPanel_Result_Search.Children) 
+            {
+                copylist.Add(item);
+            }
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                
+                string oldfilename = "";
+                RichTextBox temp = new RichTextBox();
+                FlowDocument targetdocument = new FlowDocument();
+                foreach (SearchItem item in StackPanel_Result_Search.Children)
+                {
+                    bool newname = false;
+                    Paragraph paragraphTitle = new Paragraph();
+                    Paragraph paragraphPage = new Paragraph();
+                    string separator = $"{item.Label_Page.Content}";
+                    paragraphPage.Inlines.Add(new Run(separator) { FontWeight = FontWeights.Bold });
+
+
+                    string newfilename = item.file.Name;
+                    if (newfilename != oldfilename)
+                    {
+                        string titleline = $"{newfilename}: {Environment.NewLine}";
+                        paragraphTitle.Inlines.Add(new Run(titleline) { FontWeight = FontWeights.Bold });
+
+                        //separator =  + separator;
+                        oldfilename = newfilename;
+                        newname = true;
+                    }
+                    
+                    
+
+
+
+
+
+
+                    CloneDocument(item.RichTextBox_search.Document, targetdocument);
+
+
+
+                    //targetdocument = item.RichTextBox_search.Document;
+                    if (newname)
+                    {
+                        targetdocument.Blocks.InsertBefore(targetdocument.Blocks.LastBlock, paragraphTitle);
+                    }
+                    
+                    targetdocument.Blocks.InsertBefore(targetdocument.Blocks.LastBlock, paragraphPage);
+
+
+                    //List<Block> maincontentblock = item.RichTextBox_search.Document.Blocks.ToList();
+
+                    //for (int i = 0; i < item.RichTextBox_search.Document.Blocks.Count; i++)
+
+                    //{
+
+                    //    targetdocument.Blocks.Add(item.RichTextBox_search.Document.Blocks.ElementAt(i).);
+
+                    //}
+                    //item.RichTextBox_search.Document = targetdocument;
+
+
+
+
+
+                    //foreach (Block block in item.RichTextBox_search.Document.Blocks)
+                    //{
+                    //    targetdocument.Blocks.Add(block);
+                    //}
+                    //targetdocument.Blocks.AddRange(maincontentblock);
+
+
+
+
+                    ////temp.Document.Blocks.Add(paragraphTitle);
+                    //Block[] Blockarray = new Block[item.RichTextBox_search.Document.Blocks.Count];
+                    //item.RichTextBox_search.Document.Blocks.CopyTo(Blockarray,0);
+                    //foreach (Paragraph para in Blockarray)
+                    //{
+                    //    temp.Document.Blocks.Add(para);
+                    //    temp.Document.Blocks.InsertBefore(temp.Document.Blocks.FirstBlock,paragraphTitle);
+                    //    //item.RichTextBox_search.Document.Blocks.Add(para);
+                    //}
+                    ////item.RichTextBox_search.SelectAll();
+                    ////item.RichTextBox_search.Copy();
+                    ////paragraph2.
+                    ////temp.Paste();
+                }
+
+                try
+                {
+                    TextRange range;
+                    FileStream fStream;
+                    range = new TextRange(targetdocument.ContentStart, targetdocument.ContentEnd);
+                    fStream = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                    range.Save(fStream, DataFormats.Rtf);
+                    fStream.Close();
+                    
+                    
+                }
+                catch
+                {
+
+                }
+                StackPanel_Result_Search.Children.Clear();
+                foreach (SearchItem item in copylist)
+                {
+                    StackPanel_Result_Search.Children.Add(item);
+                }
+            }
+        }
+
         #endregion
 
         private void TextBox_Directory_DoubleClick(object sender, MouseButtonEventArgs e)
@@ -146,8 +273,10 @@ namespace WPF_PDF_Organizer
                 {
                     TextBox_Directory.Text = fbd.SelectedPath;
                 }
-                
+
             }
         }
+
+
     }
 }
